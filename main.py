ad45509c5f94817d404cd71b1869bd92787445f0
meganmrichardson <megan.marie.richardson@gmail.com>
2018-08-13 16:09:59 -0700

    working on links

--- main.py
+++ main.py
@@ -9,32 +9,32 @@ jinja_env = jinja2.Environment(
 
 class FeedHandler(webapp2.RequestHandler):
     def get(self):
-        feed_template = jinja_env.get_template("templates/feed-page.html")
+        feed_template = jinja_env.get_template("templates/feed.html")
         self.response.write(feed_template.render())
-    
+
 class SettingsHandler(webapp2.RequestHandler):
     def get(self):
-        settings_template = jinja_env.get_template("templates/settings-page.html")
+        settings_template = jinja_env.get_template("templates/settings.html")
         self.response.write(settings_template.render())
 
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
-        profile_template = jinja_env.get_template("templates/profile-page.html")
+        profile_template = jinja_env.get_template("templates/profile.html")
         self.response.write(profile_template.render())
 
 class SearchHandler(webapp2.RequestHandler):
     def get(self):
-        search_template = jinja_env.get_templates("templates/search-page.html")
+        search_template = jinja_env.get_templates("templates/search.html")
         self.response.write(search_template.render())
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
-        login_template = jinja_env.get_template("templates/login-page.html")
+        login_template = jinja_env.get_template("templates/login.html")
         self.response.write(login_template.render())
 
 class NotficationsHandler(webapp2.RequestHandler):
     def get(self):
-        notifications_template = jinja_env.get_template("templates/notification-page.html")
+        notifications_template = jinja_env.get_template("templates/notification.html")
         self.response.write(notifications_template.render())
 
 app = webapp2.WSGIApplication ([
