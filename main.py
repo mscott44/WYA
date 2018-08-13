9426ee81080a032528a4601cf845acb5479f79ba
Moriah Scott <moriahscott@Moriahs-MacBook-Pro.local>
2018-08-13 15:08:00 -0700

    main.py changes

--- main.py
+++ main.py
@@ -9,22 +9,31 @@ jinja_env = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("")
+        self.response.write("Where You At?")
 
 class SettingsHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_env.get_template("templates/settings-page.html")
         self.response.write("")
+
 class ProfileHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_env.get_template("templates/profile-page.html")
         self.response.write("")
+
 class SearchHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_env.get_templates("templates/search-page.html")
         self.response.write("")
+
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_env.get_template("templates/login-page.html")
         self.response.write("")
+
 class NotficationsHandler(webapp2.RequestHandler):
     def get(self):
+        template = jinja_env.get_template("templates/notification-page.html")
         self.response.write("")
 
 app = webapp2.WSGIApplication ([
