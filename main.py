8f5fb70cb1dbd89c591911b940cf9c9328c5b174
Moriah Scott <moriahscott@Moriahs-MacBook-Pro.local>
2018-08-13 12:16:43 -0700

    more jinja

--- main.py
+++ main.py
@@ -6,3 +6,32 @@ jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("")
+
+class SettingsHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("")
+class ProfileHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("")
+class SearchHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("")
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("")
+class NotficationsHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("")
+
+app = webapp2.WSGIApplication ([
+('/', MainHandler),
+('/settings', SettingsHandler),
+('/profile', ProfileHandler),
+('/search', SearchHandler),
+('/login', LoginHandler),
+('/notifications' , NotficationsHandler)
+], debug = True)
