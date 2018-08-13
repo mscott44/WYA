3c6cc0b9a320be17fe5478106839c01938a11838
Moriah Scott <moriahscott@Moriahs-MacBook-Pro.local>
2018-08-13 12:00:08 -0700

    jinja

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import webapp2
 import os
 import jinja2
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
