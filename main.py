1508a82dbe9f86df5b7899d706e227afc2654322
meganmrichardson <megan.marie.richardson@gmail.com>
2018-08-13 17:29:17 -0700

    deleting doubles

--- main.py
+++ main.py
@@ -14,31 +14,6 @@ class FeedHandler(webapp2.RequestHandler):
 
 class SettingsHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template("templates/settings-page.html")
-        self.response.write(template.render())
-
-class ProfileHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_template("templates/profile-page.html")
-        self.response.write(template.render())
-
-class SearchHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_templates("templates/search-page.html")
-        self.response.write(template.render())
-
-class LoginHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_template("templates/login-page.html")
-        self.response.write(template.render())
-
-class NotficationsHandler(webapp2.RequestHandler):
-    def get(self):
-        template = jinja_env.get_template("templates/notification-page.html")
-        self.response.write (template.render())
-
-class SettingsHandler(webapp2.RequestHandler):
-    def get(self):
         settings_template = jinja_env.get_template("templates/settings.html")
         self.response.write(settings_template.render())
 
