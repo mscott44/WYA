317b2e549b2e56bd829065bd5ae8ae44086ec970
Elena S. Martinez <elenasmartinez22@gmail.com>
2018-08-15 00:13:29 -0700

    added a user profile picture and edited chatbox

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import webapp2
-import os
+import datetime
 import jinja2
-import time
+import json
+import os
 import webapp2
+
 from models import Post, User
 from content_management import populate_feed, logout_url, login_url
 from google.appengine.api import users
@@ -13,21 +14,20 @@ jinja_current_directory = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
-#LOGIN STUFF STARTS HERE
 class ReturningUser(ndb.Model):
-  first_name = ndb.StringProperty()
-  last_name = ndb.StringProperty()
+   first_name = ndb.StringProperty()
+   last_name = ndb.StringProperty()
 
 class LoginHandler(webapp2.RequestHandler):
-    def get(self):
+     def get(self):
 
-        new_user_template = jinja_current_directory.get_template("templates/new_user.html")
-        google_login_template = jinja_current_directory.get_template("templates/login.html")
+         new_user_template = jinja_current_directory.get_template("templates/new_user.html")
+         google_login_template = jinja_current_directory.get_template("templates/login.html")
 
-        # get Google users
-        user = users.get_current_user()
+       #get Google users
+         user = users.get_current_user()
 
-        if user:
+         if user:
             # look for user in datastore
             existing_user = User.query().filter(User.email == user.email()).get()
             nickname = user.nickname()
@@ -40,71 +40,122 @@ class LoginHandler(webapp2.RequestHandler):
                 self.response.write(new_user_template.render(fields))
             else:
                 # direct existing user to feed_template
-                self.redirect('/feed')
-        else:
-            # ask user to sign in to google
-            self.response.write(google_login_template.render({ "login_url": login_url }))
+               self.redirect('/feed')
+         else:
+           # ask user to sign in to google
+           self.response.write(google_login_template.render({ "login_url": login_url }))
 
-#LOGIN STUFF ENDS HERE
-
+ #LOGIN STUFF ENDS HERE
 class FeedHandler(webapp2.RequestHandler):
-    def get(self):
-        feed_template = jinja_current_directory.get_template("templates/feed.html")
-        self.response.write(feed_template.render({ "sign_out": logout_url }))
-
-    def post(self):
-        user = users.get_current_user()
-        if user is None:
-            self.redirect('/')
-            return # lol idk if this is ok?? it works I guess
-        current_user = User.query().filter(User.email == user.email()).get()
-        if not current_user:
-            # upon new user form submission, create new user and store in datastore
-            new_user_entry = User(
-                name = self.request.get("name"),
-                username = self.request.get("username"),
-                email = user.email(),
-            )
-            new_user_entry.put()
-            current_user = new_user_entry
-        else:
-            # if not a new user, existing user submitted a post from feed
-            new_post = Post(author= current_user.key, content= self.request.get("user_post"))
-            new_post.put()
-        time.sleep(.2)
-        self.redirect('/feed')
-
+   def get(self):
+       user = users.get_current_user()
+       current_user = User.query().filter(User.email == user.email()).get()
+       fields = {
+            "username": current_user.username,
+            "sign_out": logout_url,
+            "email" : current_user.email
+        }
+       feed_template = jinja_current_directory.get_template("templates/feed.html")
+       self.response.write(feed_template.render(fields))
+   def post(self):
+         user = users.get_current_user()
+         if user is None:
+             self.redirect('/')
+             return # lol idk if this is ok?? it works I guess
+         current_user = User.query().filter(User.email == user.email()).get()
+         if not current_user:
+             # upon new user form submission, create new user and store in datastore
+             new_user_entry = User(
+                 name = self.request.get("name"),
+                 username = self.request.get("username"),
+                 email = user.email(),
+             )
+             new_user_entry.put()
+             current_user = new_user_entry
+         else:
+             # if not a new user, existing user submitted a post from feed
+             new_post = Post(author= current_user.key, content= self.request.get("user_post"))
+             new_post.put()
+         time.sleep(.2)
+         self.redirect('/feed')
 class SettingsHandler(webapp2.RequestHandler):
-    def get(self):
-        settings_template = jinja_current_directory.get_template("templates/settings.html")
-        self.response.write(settings_template.render({ "sign_out": logout_url }))
+     def get(self):
+         settings_template = jinja_current_directory.get_template("templates/settings.html")
+         self.response.write(settings_template.render({ "sign_out": logout_url }))
 
 class ChatroomHandler(webapp2.RequestHandler):
-    def get(self):
-        settings_template = jinja_current_directory.get_template("templates/chatroom.html")
-        self.response.write(settings_template.render({ "sign_out": logout_url}))
+     def get(self):
+         settings_template = jinja_current_directory.get_template("templates/chatroom.html")
+         self.response.write(settings_template.render({ "sign_out": logout_url}))
 
 class ProfileHandler(webapp2.RequestHandler):
-    def get(self):
-        profile_template = jinja_current_directory.get_template("templates/profile.html")
-        self.response.write(profile_template.render({ "sign_out": logout_url }))
+     def get(self):
+         profile_template = jinja_current_directory.get_template("templates/profile.html")
+         self.response.write(profile_template.render({ "sign_out": logout_url }))
 
 class SearchHandler(webapp2.RequestHandler):
-    def get(self):
-        search_template = jinja_current_directory.get_template("templates/search.html")
-        self.response.write(search_template.render({ "sign_out": logout_url }))
+     def get(self):
+         search_template = jinja_current_directory.get_template("templates/search.html")
+         self.response.write(search_template.render({ "sign_out": logout_url }))
 
 class NotficationsHandler(webapp2.RequestHandler):
+     def get(self):
+         notifications_template = jinja_current_directory.get_template("templates/notifications.html")
+         self.response.write(notifications_template.render({ "sign_out": logout_url }))
+
+class Message(ndb.Model):
+    content = ndb.StringProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
+    @classmethod
+    def query_conversation(cls, ancestor_key):
+        return cls.query(ancestor=ancestor_key).order(cls.timestamp)
+
+class ChatService(webapp2.RequestHandler):
     def get(self):
-        notifications_template = jinja_current_directory.get_template("templates/notifications.html")
-        self.response.write(notifications_template.render({ "sign_out": logout_url }))
+        key = self.getKey(self.request);
+        ancestor_key = ndb.Key('Messages', key)
+        messages = Message.query_conversation(ancestor_key).fetch()
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(
+            json.dumps([self.to_serializable(m) for m in messages]))
+
+    def post(self):
+        key = self.getKey(self.request);
+        content = self.request.get('content');
+        message = Message(parent=ndb.Key("Messages", key), content=content)
+        message.put()
+
+    def getKey(self, request):
+        from_user = self.request.get('from');
+        to_user = self.request.get('to');
+        key_values = [from_user, to_user]
+        key_values.sort()
+        return key_values[0] + '_' + key_values[1];
+
+    def to_serializable(self, data):
+        """Build a new dict so that the data can be JSON serializable"""
+        result = data.to_dict()
+        record = {}
+
+        for key in result.iterkeys():
+            if isinstance(result[key], datetime.datetime):
+                record[key] = result[key].isoformat()
+                continue
+            record[key] = result[key]
+
+        record['key'] = data.key.id()
+        return record
+
+
 
 app = webapp2.WSGIApplication ([
+('/chat', ChatService),
 ('/', LoginHandler),
 ('/chatroom', ChatroomHandler),
 ('/feed', FeedHandler),
 ('/settings', SettingsHandler),
 ('/profile', ProfileHandler),
 ('/search', SearchHandler),
-('/notifications' , NotficationsHandler)
+('/notifications' , NotficationsHandler),
 ], debug = True)
