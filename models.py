b642a7b4ed4b2cab223e6ba6f4722da7a96a8b29
meganmrichardson <megan.marie.richardson@gmail.com>
2018-08-16 12:04:19 -0700

    ordering the users by time they made account to fill the feed page

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class User(ndb.Model):
     name = ndb.StringProperty(required=True)
     username = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
+    time = ndb.DateTimeProperty(auto_now_add=True)
 
 class Post(ndb.Model):
     author = ndb.KeyProperty(User, required=True)
